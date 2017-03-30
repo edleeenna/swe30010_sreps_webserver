991fae642c37fbadb2c6eb95d0038201151d5dd7
Jyden Needs <jrneeds@outlook.com>
2017-03-30 14:36:16 +1100

    Update phpmysql.php

--- phpmysql.php
+++ phpmysql.php
@@ -16,22 +16,17 @@ foreach ($_SERVER as $key => $value) {
     $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-define('DB_SERVERNAME', $servername);
-define('DB_NAME', $dbname);
-define('DB_USERNAME', $username);
-define('DB_PASSWORD', $password);
-
 // Create connection
-$conn = new mysqli(DB_SERVERNAME, 'pharmacy', DB_PASSWORD, DB_NAME);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "connection successful<br/>";
-    echo "DB_SERVERNAME: $servername<br/>";
-    echo "DB_NAME: $dbname<br/>";
-    echo "DB_USERNAME: $username<br/>";
-    echo "DB_PASSWORD: $password<br/>";
+ //   echo "DB_SERVERNAME: $servername<br/>";
+ //   echo "DB_NAME: $dbname<br/>";
+ //   echo "DB_USERNAME: $username<br/>";
+ //   echo "DB_PASSWORD: $password<br/>";
 }
 $conn->close();
 ?>
