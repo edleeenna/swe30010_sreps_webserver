46acfdec62a41d17d3aa8425cfedae8523aa528a
JohnVin <2050056@student.swin.edu.au>
2017-03-31 16:27:13 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>SWE30010 - Development Project 2</p>'; ?> 
- </body>
-</html>
+<?php
+  include 'includes/head.php';
+?>
+    Page content Start<br>
+    Page content goes here<br>
+    <?php echo '<p>SWE30010 - Development Project 2</p>'; ?>
+    Page content End<br>
+<?php
+  include 'includes/tail.php';
+?>
