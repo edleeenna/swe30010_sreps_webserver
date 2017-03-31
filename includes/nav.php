cbac04902737d503aa713af449a826b9f04d8470
JohnVin <2050056@student.swin.edu.au>
2017-03-31 16:21:12 +1100

    Create nav.php
    Created basic navigation John.

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+    <nav>
+      <a href="">Add Stock Item</a>, 
+      <a href="">Edit Stock Item</a>, 
+      <a href="">Display Stock Item</a>, 
+      <a href="">Delete Stock Item</a>, 
+      <a href="">Add Sale</a>, 
+      <a href="">Edit Sale</a>, 
+      <a href="">Display Sale</a>.
+    </nav>
