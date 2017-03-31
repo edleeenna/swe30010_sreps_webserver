e625dc118f9389114d7f380d409f455e159b5c1f
JohnVin <2050056@student.swin.edu.au>
2017-03-31 16:30:50 +1100

    Create page_template_copy_to_.php

---
+++ page_template_copy_to_.php
@@ -0,0 +1,10 @@
+<?php
+  include 'includes/head.php';
+?>
+    Page content Start<br>
+    Page content goes here<br>
+    Take out this text<br>
+    Page content End<br>
+<?php
+  include 'includes/tail.php';
+?>
