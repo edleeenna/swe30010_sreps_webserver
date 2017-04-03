867cf970a0fc0a7d0569103eb8428e551c676e52
JohnVin <2050056@student.swin.edu.au>
2017-04-03 21:11:48 +1000

    Update edit_stock.php
    Created base elements to Edit Stock Item page. John.

--- edit_stock.php
+++ edit_stock.php
@@ -1,1 +1,14 @@
-
+<?php
+  include 'includes/head.php';
+?>
+    Edit Stock Item<br>
+    Item<br>
+    ID:<br>
+    Name:<br>
+    Cost Price<br>
+    Price <br>
+    Quantity <br>
+    Minimm Target Quantity:<br>
+<?php
+  include 'includes/tail.php';
+?>
