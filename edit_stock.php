a9bbbdf1ab4cffefd3d8ce41eecfb04c946a953a
JohnVin <2050056@student.swin.edu.au>
2017-04-05 14:23:41 +1000

    Update edit_stock.php
    Created the edit stock item form with basic details. John.

--- edit_stock.php
+++ edit_stock.php
@@ -2,7 +2,9 @@
   echo "Start<br>".PHP_EOL;
   include 'includes/head.php';
   echo "connect<br>".PHP_EOL;
-  
+  // Include functions for editing stock. Could be made part of all functions for stock. eg: stock_func.php
+  include 'includes/stock_func_edit.php';
+
   function get_stock_item(){
     
     include 'includes/db_connect.php';
@@ -38,8 +40,36 @@
     echo "Close<br>".PHP_EOL;
     $conn->close();
   }
-  get_stock_item();
+  //get_stock_item();
+  if (!isset($POST['stock_select'])) {
 ?>
+    <form id="stock_item" action="edit_stock.php" method="post">
+      Select stock item to edit, by ID or Name.<br>
+      <select name="ID_list">
+        get_ID_list();
+      </select>
+<?php /*
+      <select name="name_list">
+        get_name_list();
+      </select>*/
+?>
+      <input type="submit" name="item_select" value="Edit Stock Item"> <input type="reset" value="Reset selection.">
+<?php
+  }
+  else{
+    $stock_details = get_stock_item_details($_POST['ID_list']);
+    //$stock_details = get_stock_item_details($_POST['name_list']);
+    // onsubmit="return check_stock_details(this)"
+?>
+      <form id="stock_item" action="edit_stock.php" method="post">
+        Item ID: <input class="stock_edit" type="text" name="item_id" value="<?php echo $php_stock_details['item_id'];?>"><br>
+        Item Name: <input class="stock_edit" type="text" name="item_name" value="<?php echo $php_stock_details['item_name'];?>"><br>
+        Item Description: <input class="stock_edit" type="text" name="item_description" value="<?php echo $php_stock_details['item_description'];?>"><br>
+        <input type="submit" name="item_update" value="Update Stock Item"> <input type="reset" value="Clear fields.">
+<?php
+  }
+?>
+    </form>
     Edit Stock Item<br>
     Item<br>
     ID:<br>
