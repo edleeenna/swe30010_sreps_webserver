68da8bbeac796a2af86820d6efa6cfe54d8d757d
JohnVin <2050056@student.swin.edu.au>
2017-04-05 12:02:11 +1000

    Update edit_stock.php
    Added basic db query to edit. John.

--- edit_stock.php
+++ edit_stock.php
@@ -1,5 +1,33 @@
 <?php
   include 'includes/head.php';
+  include 'includes/db_connect.php';
+
+  try{
+    // Test if any records match the supplied username and password
+    $sql = "SELECT * FROM stock;";
+    // Query the database to acquire results and hand them to resultSet
+    $result = $conn->query($sql);
+  }
+  catch(PDOEXCEPTION $e){
+    // Display error message details
+    echo 'Error fetching user login details: '.$e->getMessage();
+    // Stop running script
+    exit();
+  }
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      //echo "ID: " . $row["item_id"]. " - Name: " . $row["item_name"]. " - Description: " . $row["item_description"]. "<br>";
+      print_r (array_values($row))
+      echo "<br>";
+    }
+  }
+  else {
+    echo "0 results";
+  }
+  
+  $conn->close();
 ?>
     Edit Stock Item<br>
     Item<br>
