5b4e05b3ef3bedce484e9bec4efc95956fc52d91
JohnVin <2050056@student.swin.edu.au>
2017-04-05 12:17:14 +1000

    Update edit_stock.php

--- edit_stock.php
+++ edit_stock.php
@@ -2,38 +2,42 @@
   echo "Start<br>".PHP_EOL;
   include 'includes/head.php';
   echo "connect<br>".PHP_EOL;
-  /*
-  include 'includes/db_connect.php';
-  echo "SQL<br>".PHP_EOL;
   
-  try{
-    echo "try<br>".PHP_EOL;
-    // Test if any records match the supplied username and password
-    $sql = "SELECT * FROM stock;";
-    // Query the database to acquire results and hand them to resultSet
-    $result = $conn->query($sql);
-  }
-  catch(PDOEXCEPTION $e){
-    echo "catch<br>".PHP_EOL;
-    // Display error message details
-    echo 'Error fetching user login details: '.$e->getMessage();
-    // Stop running script
-    exit();
-  }
-  echo "results<br>".PHP_EOL;
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      //echo "ID: " . $row["item_id"]. " - Name: " . $row["item_name"]. " - Description: " . $row["item_description"]. "<br>";
-      print_r (array_values($row))
-      echo "<br>";
+  function get_stock_item(){
+    
+    include 'includes/db_connect.php';
+    echo "SQL<br>".PHP_EOL;
+
+    try{
+      echo "try<br>".PHP_EOL;
+      // Test if any records match the supplied username and password
+      $sql = "SELECT * FROM stock;";
+      // Query the database to acquire results and hand them to resultSet
+      $result = $conn->query($sql);
     }
+    catch(PDOEXCEPTION $e){
+      echo "catch<br>".PHP_EOL;
+      // Display error message details
+      echo 'Error fetching user login details: '.$e->getMessage();
+      // Stop running script
+      exit();
+    }
+    echo "results<br>".PHP_EOL;
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        //echo "ID: " . $row["item_id"]. " - Name: " . $row["item_name"]. " - Description: " . $row["item_description"]. "<br>";
+        print_r (array_values($row))
+        echo "<br>";
+      }
+    }
+    else {
+      echo "0 results";
+    }
+    echo "Close<br>".PHP_EOL;
+    $conn->close();
   }
-  else {
-    echo "0 results";
-  }
-  echo "Close<br>".PHP_EOL;
-  $conn->close();*/
+  get_stock_item();
 ?>
     Edit Stock Item<br>
     Item<br>
