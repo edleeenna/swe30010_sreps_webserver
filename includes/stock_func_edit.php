790e6e0fe8703cc3099db23d36ad6dd3412c79b1
JohnVin <2050056@student.swin.edu.au>
2017-04-05 18:09:35 +1000

    Update stock_func_edit.php

--- includes/stock_func_edit.php
+++ includes/stock_func_edit.php
@@ -57,9 +57,9 @@
         print_r (array_values($row))
         echo "<br>";
       }*/
-    echo "Result testing completed.<br>".PHP_EOL;
+    echo "Result testing completed.".$result->num_rows."<br>".PHP_EOL;
     //$row = $result->fetch();
-    $row = $results->fetch_assoc();
+    $row = $result->fetch_assoc();
     print_r (array_values($row));
     $php_stock_details = array();
     $php_stock_details[0] = $row['stock_id'];
