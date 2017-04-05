333cb75dfd334544ec18080866cf3023cf93d064
JohnVin <2050056@student.swin.edu.au>
2017-04-05 16:46:05 +1000

    Update edit_stock.php

--- edit_stock.php
+++ edit_stock.php
@@ -5,42 +5,6 @@
   // Include functions for editing stock. Could be made part of all functions for stock. eg: stock_func.php
   include 'includes/stock_func_edit.php';
 
-  function get_stock_item(){
-    
-    include 'includes/db_connect.php';
-    echo "SQL<br>".PHP_EOL;
-
-    try{
-      echo "try<br>".PHP_EOL;
-      // Test if any records match the supplied username and password
-      $sql = "SELECT * FROM stock;";
-      // Query the database to acquire results and hand them to resultSet
-      $result = $conn->query($sql);
-    }
-    catch(PDOEXCEPTION $e){
-      echo "catch<br>".PHP_EOL;
-      // Display error message details
-      echo 'Error fetching user login details: '.$e->getMessage();
-      // Stop running script
-      exit();
-    }
-    echo "results<br>".PHP_EOL;
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-      //while($row = $result->fetch()) {
-        //echo "ID: " . $row["item_id"]. " - Name: " . $row["item_name"]. " - Description: " . $row["item_description"]. "<br>";
-        print_r (array_values($row));
-        echo "<br><br>".PHP_EOL;
-      }
-    }
-    else {
-      echo "0 results";
-    }
-    echo "Close<br>".PHP_EOL;
-    $conn->close();
-  }
-  //get_stock_item();
   if (!isset($POST['item_select'])) {
 ?>
     <form id="stock_item" action="edit_stock.php" method="post">
