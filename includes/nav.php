2f9db2757fd38a9405649dc570c101e9c22dc78c
2040883 <2040883@student.swin.edu.au>
2017-04-07 16:55:37 +1000

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,15 @@
-    <nav>
-      <a href="./index.php">Home</a>
-      <a href="./addstock.php">Add Stock Item</a>
-      <a href="./edit_stock.php">Edit Stock Item</a>
-      <a href="./displaystock.php">Display Stock Item</a>
-      <a href="">Delete Stock Item</a>
-      <a href="">Add Sale</a>
-      <a href="">Edit Sale</a>
-      <a href="">Display Sale</a><br>
-     </nav>
+ <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">PHP SRePS</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="./addstock.php">Add Stock</a></li>
+        <li><a href="./edit_stock.php">Edit Stock</a></li>
+        <li><a href="./displaystock.php">Display Stock</a></li>
+        <li><a href="">Delete Stock</a></li>
+        <li><a href="">Add Sale</a></li>
+        <li><a href="">Edit Sale</a></li>
+        <li><a href="">Display Sale</a></li>
+      </ul>
+    </div>
+  </nav>
