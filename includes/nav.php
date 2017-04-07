7eaa7f43661fa8a3784790243624329d9e5f01a3
2040883 <2040883@student.swin.edu.au>
2017-04-07 16:41:38 +1000

    Nav Bar
    Test change to Nav bar layout

--- includes/nav.php
+++ includes/nav.php
@@ -1,12 +1,10 @@
     <nav>
-      <a href="./index.php">Home</a>,
-      <a href="./addstock.php">Add Stock Item</a>,
-      <a href="./edit_stock.php">Edit Stock Item</a>,
-      <a href="./displaystock.php">Display Stock Item</a>,
-      <a href="">Delete Stock Item</a>,
-      <a href="">Add Sale</a>,
-      <a href="">Edit Sale</a>,
-      <a href="">Display Sale</a>.<br>
-      <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
-      <hr>
-    </nav>
+      <a href="./index.php">Home</a>
+      <a href="./addstock.php">Add Stock Item</a>
+      <a href="./edit_stock.php">Edit Stock Item</a>
+      <a href="./displaystock.php">Display Stock Item</a>
+      <a href="">Delete Stock Item</a>
+      <a href="">Add Sale</a>
+      <a href="">Edit Sale</a>
+      <a href="">Display Sale</a><br>
+     </nav>
