948c7bbfd7f1fa048f49a3bbb9beb53e85c5143f
JohnVin <2050056@student.swin.edu.au>
2017-04-07 10:43:37 +1000

    Update stock_func_edit.php
    Updated to also do SQL update of stock, not all fields, oops. John.

--- includes/stock_func_edit.php
+++ includes/stock_func_edit.php
@@ -92,7 +92,21 @@
       try{
         // Test if any records match the supplied username and password
         //$sql = "UPDATE stock SET stock_id = :stock_id, stock_name = :stock_name, stock_description = :stock_description;";
-        $sql = "UPDATE stock SET stock_id = ".cleanInput($_POST['html_stock_id']).", stock_name = ".cleanInput($_POST['html_stock_name']).", stock_description = ".cleanInput($_POST['html_stock_description']).";";
+        $sql = "UPDATE stock SET";
+        $sql .= "stock_id = ".cleanInput($_POST['html_stock_id']);
+        $sql .= ", stock_name = ".cleanInput($_POST['html_stock_name']);
+        $sql .= ", stock_description = ".cleanInput($_POST['html_stock_description']);
+        $sql .= ", stock_directions = ".cleanInput($_POST['html_stock_directions']);
+        $sql .= ", stock_ingredients = ".cleanInput($_POST['html_stock_ingredients']);
+        $sql .= ", stock_price = ".cleanInput($_POST['html_stock_price']);
+        $sql .= ", stock_cost_price = ".cleanInput($_POST['html_stock_cost_price']);
+        $sql .= ", stock_qty = ".cleanInput($_POST['html_stock_qty']);
+        $sql .= ", stock_target_min_qty = ".cleanInput($_POST['html_stock_target_min_qty']);
+        $sql .= ", stock_supplier = ".cleanInput($_POST['html_stock_supplier']);
+        $sql .= ", stock_supplier_order_code = ".cleanInput($_POST['html_stock_supplier_order_code']);
+        $sql .= ", stock_category_id = ".cleanInput($_POST['html_stock_category_id']);
+        $sql .= ", stock_bar_code = ".cleanInput($_POST['html_stock_bar_code']);
+        $sql .= ";";
       //echo "SQL completed.<br>".PHP_EOL;
         
         // Prepare sql statement
