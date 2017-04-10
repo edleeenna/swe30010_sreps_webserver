09c0201d5b05a2085622bf7545d29ab91cfcad86
jazonzos <1778064@student.swin.edu.au>
2017-04-10 21:45:37 +1000

    Add export function

--- stockexportprocess.php
+++ stockexportprocess.php
@@ -5,7 +5,33 @@ include 'includes/db_connect.php';
   
 $select = "SELECT * FROM stock";
 
-$result = mysqli_query($conn, $select);
+$result = $conn->query($select);
+
+
+function stockExport($result){
+  $f = fopen('php://temp', 'wt');
+  $first = true;
+  while ($row = $result->fetch_assoc()) {
+    if ($first) {
+      fputcsv($f, array_keys($row));
+      $first = false;
+    }
+    fputcsv($f, $row);
+  }
+
+  $size = ftell($f);
+  $date = date("Y-m-d");
+  rewind($f);
+  header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+  header("Content-Length: $size");
+  header("Content-type: text/x-csv");
+  header("Content-type: text/csv");
+  header("Content-type: application/csv");
+  header("Content-Disposition: attachment; filename=".$date.".csv");
+  fpassthru($f);
+  exit;
+}
+
 
 if (!$conn) {
 
@@ -16,44 +42,11 @@ else {
     echo("Error description: " . mysqli_error($conn));
   }
 else {
-   
-  $fields = mysqli_num_fields ( $result );
+   stockExport($result);
 
-  for ( $i = 0; $i < $fields; $i++ ){
-    $header .= mysqli_fetch_field_direct($result, $i)->name. "\t";
-  }
-
-  while( $row = mysqli_fetch_row( $result ) ){
-    $line = '';
-    foreach( $row as $value ){                                            
-    if ( ( !isset( $value ) ) || ( $value == "" ) ){
-      $value = "\t";
-    }
-    else {
-      $value = str_replace( '"' , '""' , $value );
-      $value = '"' . $value . '"' . "\t";
-    }
-      $line .= $value;
-    }
-    $data .= trim( $line ) . "\n";
-  }
-
-  $data = str_replace( "\r" , "" , $data );
-  if ( $data == "" ){
-    $data = "\n(0) Records Found!\n";                        
-  }
-
-  $date = date("Y-m-d");
-
-  header("Content-type: application/octet-stream");
-  header("Content-Disposition: attachment; filename=".$date.".xls");
-  header("Pragma: no-cache");
-  header("Expires: 0");
-  print "$header\n$data";
   }
 }
-
-mysqli_close($conn);
+$conn->close();
 
   include 'includes/tail.php';
 ?>
