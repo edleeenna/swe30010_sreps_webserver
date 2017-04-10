4ed1560db32a074fb07cdfb5e40b04ba3288d709
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:35:48 +1000

    Update edit_stock_levels.js
    Changed to have more than just test. John.

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -1,9 +1,9 @@
 alert('edit_stock_level.js loaded');
-//function focusgain(e){
-//  e.classList.add('focused'), true);
-//}
+function focusgain(e){
+  e.classList.add('focused'), true);
+}
 //function focuslost(e){
 //  e.classList.remove('focused'), true);
 //}
-//document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
+document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
 //document.getElementByClassName("TCR").addEventListener('blur', focuslost(this));
