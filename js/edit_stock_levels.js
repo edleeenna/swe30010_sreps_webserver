a0bcfa45de39425a30944db00e1c2efba6626eb8
JohnVin <2050056@student.swin.edu.au>
2017-04-10 16:27:32 +1000

    Update edit_stock_levels.js

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -1,19 +1,19 @@
 //alert('edit_stock_level.js loaded');
 
 function focusgain(e){
-  alert('gained focus');
-  //e.classList.add('focused'), true);
+  //alert('gained focus');
+  e.classList.add('focused'), true);
 }
 
 function focusgone(e){
-  alert('focus lost');
-  //e.classList.remove('focused'), true);
+  //alert('focus lost');
+  e.classList.remove('focused'), true);
 }
 var pieces = document.getElementsByClassName("TCR");
 //alert(pieces);
 var mylist ='';
 for (var i = 0; i < pieces.length; i++) {
-  mylist += pieces[i];
+  mylist += pieces[i].name;
   pieces[i].addEventListener('focus', focusgain(this), true);
   pieces[i].addEventListener('blur', focusgone(this), true);
 }
