191b89ac734bc31adfa05f4a0ebd7298b4ca8037
Jyden Needs <jrneeds@outlook.com>
2017-04-10 18:13:58 +1000

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     Page content goes here<br>
     <?php echo '<p>SWE30010 - Development Project 2</p>'; ?>
     Page content End<br>
+    <a class="waves-effect waves-light btn-large">Quick Sale</a>
+    <a class="waves-effect waves-light btn-large">Manage Stock</a>
+    <a class="waves-effect waves-light btn-large">Predictions</a>
+    <a class="waves-effect waves-light btn-large">Support</a>
 <?php
   include 'includes/tail.php';
 ?>
