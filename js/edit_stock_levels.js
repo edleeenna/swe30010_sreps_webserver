27e5e78f11b434e5286220162ce99883a0bf1ca1
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:20:25 +1000

    Create edit_stock_levels.js
    Created javascript and function for handling editable cells in edit_stock_levels. John.

---
+++ js/edit_stock_levels.js
@@ -0,0 +1,8 @@
+function focusgain(e){
+  e.classList.add('focused'), true);
+}
+function focuslost(e){
+  e.classList.remove('focused'), true);
+}
+document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
+document.getElementByClassName("TCR").addEventListener('blur', focuslost(this));
