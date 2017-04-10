795464340776dd004205cb6be5a549c295afaced
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:43:26 +1000

    Update edit_stock_levels.js
    Adjusted event addition. John.

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -2,8 +2,11 @@ alert('edit_stock_level.js loaded');
 function focusgain(e){
   e.classList.add('focused'), true);
 }
-//function focuslost(e){
-//  e.classList.remove('focused'), true);
-//}
-document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
-//document.getElementByClassName("TCR").addEventListener('blur', focuslost(this));
+function focuslost(e){
+  e.classList.remove('focused'), true);
+}
+var pieces document.getElementsByClassName("TCR");
+for (var i = 0; i < pieces.length; i++) {
+  pieces[i].addEventListener('focus', focusgain(this));
+  pieces[i].addEventListener('blur', focuslost(this));
+}
