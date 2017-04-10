a8ee787c1593fb467cfea0e1d5b9622d07856e96
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:49:24 +1000

    Update edit_stock_levels.js
    Basic functions. John.

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -1,15 +1,17 @@
 alert('edit_stock_level.js loaded');
 
 function focusgain(e){
-  e.classList.add('focused'), true);
+  alert('gained focus');
+  //e.classList.add('focused'), true);
 }
 function focuslost(e){
-  e.classList.remove('focused'), true);
+  alert('focus lost');
+  //e.classList.remove('focused'), true);
 }
 var pieces = document.getElementsByClassName("TCR");
 //alert(pieces);
 
 for (var i = 0; i < pieces.length; i++) {
-  pieces[i].addEventListener('focus', focusgain(this));
-  pieces[i].addEventListener('blur', focuslost(this));
+  pieces[i].addEventListener('focus', focusgain());
+  pieces[i].addEventListener('blur', focuslost());
 }
