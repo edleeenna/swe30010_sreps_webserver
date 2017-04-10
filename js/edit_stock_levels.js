84ffcf33f3766c4003a903220c45dc80f2575cd5
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:45:32 +1000

    Update edit_stock_levels.js

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -1,12 +1,15 @@
 alert('edit_stock_level.js loaded');
+/*
 function focusgain(e){
   e.classList.add('focused'), true);
 }
 function focuslost(e){
   e.classList.remove('focused'), true);
-}
+} */
 var pieces document.getElementsByClassName("TCR");
+alert(pieces);
+/*
 for (var i = 0; i < pieces.length; i++) {
   pieces[i].addEventListener('focus', focusgain(this));
   pieces[i].addEventListener('blur', focuslost(this));
-}
+}*/
