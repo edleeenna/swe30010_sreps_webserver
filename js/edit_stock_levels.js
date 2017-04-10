6bd7c53fe44bf06c89421bb0bd82c1be03f50806
JohnVin <2050056@student.swin.edu.au>
2017-04-10 15:33:18 +1000

    Update edit_stock_levels.js
    Changed javascript to testing. John.

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -1,8 +1,9 @@
-function focusgain(e){
-  e.classList.add('focused'), true);
-}
-function focuslost(e){
-  e.classList.remove('focused'), true);
-}
-document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
-document.getElementByClassName("TCR").addEventListener('blur', focuslost(this));
+alert('edit_stock_level.js loaded');
+//function focusgain(e){
+//  e.classList.add('focused'), true);
+//}
+//function focuslost(e){
+//  e.classList.remove('focused'), true);
+//}
+//document.getElementByClassName("TCR").addEventListener('focus', focusgain(this));
+//document.getElementByClassName("TCR").addEventListener('blur', focuslost(this));
