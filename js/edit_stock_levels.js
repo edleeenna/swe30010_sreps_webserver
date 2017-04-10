4b93f5d0c4c6ba6ed3bbd32915430061187fccbf
JohnVin <2050056@student.swin.edu.au>
2017-04-10 16:38:25 +1000

    Update edit_stock_levels.js

--- js/edit_stock_levels.js
+++ js/edit_stock_levels.js
@@ -14,7 +14,7 @@ var pieces = document.getElementsByClassName("TCR");
 //alert(pieces);
 var mylist ='';
 for (var i = 0; i < pieces.length; i++) {
-  mylist += pieces[i].name;
+  mylist += pieces[i].localName+' '+pieces[i].innerText;
   pieces[i].addEventListener('focus', focusgain(this), true);
   pieces[i].addEventListener('blur', focusgone(this), true);
 }
