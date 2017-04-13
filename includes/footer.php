e1d0b61a4fd59d12760f8626629f060dbe116c4a
JohnVin <2050056@student.swin.edu.au>
2017-04-13 19:53:48 +1000

    Update footer.php
    Minor corrections. John.

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     <footer>
       <hr>
       Pharmacy Sales Reporting and Prediction System for SWE30010 - Development Project 2 - Semester 1, 2017. Group 0.<br>
-      <a href="" text="Privacy policy. &lt;Place Holder&gt;">Privacy policy</a>, <a href="" text="Disclaimer. &lt;Place Holder&gt;">Disclaimer</a>,
-        <a href="" text="Contact Us. &lt;Place Holder&gt;">Contact Us</a><a href="" text="Copyright. &lt;Place Holder&gt;">Copyright</a>
+      <a href="" title="Privacy policy. &lt;Place Holder&gt;">Privacy policy</a>, <a href="" title="Disclaimer. &lt;Place Holder&gt;">Disclaimer</a>,
+        <a href="" title="Contact Us. &lt;Place Holder&gt;">Contact Us</a>, <a href="" title="Copyright. &lt;Place Holder&gt;">Copyright</a>.
     </footer>
