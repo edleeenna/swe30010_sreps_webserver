47c3e0d833d8254c11d709a9299dc675d66b7f99
JohnVin <2050056@student.swin.edu.au>
2017-04-13 19:52:24 +1000

    Update footer.php
    Updated footer to be able to remove unnecessary lines from other pages. John.

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
     <footer>
       <hr>
-      Footer Text here can be modified later.
+      Pharmacy Sales Reporting and Prediction System for SWE30010 - Development Project 2 - Semester 1, 2017. Group 0.<br>
+      <a href="" text="Privacy policy. &lt;Place Holder&gt;">Privacy policy</a>, <a href="" text="Disclaimer. &lt;Place Holder&gt;">Disclaimer</a>,
+        <a href="" text="Contact Us. &lt;Place Holder&gt;">Contact Us</a><a href="" text="Copyright. &lt;Place Holder&gt;">Copyright</a>
     </footer>
