deeca3fbc46872bc9427b59821b38f56013c529d
2040883 <2040883@student.swin.edu.au>
2017-04-13 23:42:22 +1000

    Footer update test -DE
    Testing Footer format

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,28 @@
-    <footer>
-      <hr>
-      Pharmacy Sales Reporting and Prediction System for SWE30010 - Development Project 2 - Semester 1, 2017. Group 0.<br>
-      <a href="" title="Privacy policy. &lt;Place Holder&gt;">Privacy policy</a>, <a href="" title="Disclaimer. &lt;Place Holder&gt;">Disclaimer</a>,
-        <a href="" title="Contact Us. &lt;Place Holder&gt;">Contact Us</a>, <a href="" title="Copyright. &lt;Place Holder&gt;">Copyright</a>.
-    </footer>
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">PHP SRePS</h5>
+                <p class="grey-text text-lighten-4">Pharmacy Sales Reporting and Prediction System for SWE30010 - Development Project 2 - Semester 1, 2017. Group 0</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a>
+                    </li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Disclaimer</a>
+                    </li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Copyright</a>
+                    </li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2017 Swinburne
+        </div>
+    </div>
+</footer>
