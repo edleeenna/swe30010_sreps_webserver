6f8e5059dd63960ef9a7ed7b913a9a37811d9e2c
a5c3nd3d <jrneeds@outlook.com>
2017-04-14 21:24:14 +1000

    Attempt fix for includes

--- includes/head.php
+++ includes/head.php
@@ -2,20 +2,20 @@
 <html>
 
 <head>
-	<link rel="icon" type="image/png" href="/wwwroot/images/medicine-32x32.png" sizes="32x32"/>
+	<link rel="icon" type="image/png" href="../images/medicine-32x32.png" sizes="32x32"/>
 	<!--<link rel="icon" type="image/png" href="/wwwroot/images/medicine-16x16.png" sizes="16x16" />-->
 
 	<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link type="text/css" rel="stylesheet" href="/wwwroot/css/common.css"/>
-	<link type="text/css" rel="stylesheet" href="/wwwroot/css/materialize.min.css" media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="css/common.css"/>
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
 
 	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="/wwwroot/js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
 </head>
 
 <body>
 	<?php
-	include $_SERVER[ 'DOCUMENT_ROOT' ] . '/wwwroot/includes/header.php';
+	include 'includes/header.php';
 	?>
 </body>
 
