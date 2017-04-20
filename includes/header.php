025c356e527a992b6ddb52674b684d834531c409
JohnVin <2050056@student.swin.edu.au>
2017-04-20 21:17:03 +1000

    Update header.php
    Updated for different header structure. John.

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,5 @@
-    <header>
       <div class="container">
         <div class="nav-wrapper">
           <a href="#!" class="brand-logo"><img src="/images/medicine-512x512.png" alt="" style="width:32px;height:32px;">PHP - SRePS</a>
         </div>
       </div>
-    </header>
