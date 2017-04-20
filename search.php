9b41659f4bc538e6adc53e7d8e343cc283e3d556
JohnVin <2050056@student.swin.edu.au>
2017-04-20 21:24:40 +1000

    Update search.php
    Updated for different page structure. John.

--- search.php
+++ search.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html>
+<?php
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
+?>
+    </nav>
 
-<header>
-	<title>Search - PHP - SRePS</title>
-	<nav class="nav-extended">
-		<?php
-		include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
-		include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
-		?>
-	</nav>
-</header>
+    <main>
+      Search
+    </main>
 
-<main>
-	<body>
-		Search
-	</body>
-</main>
-
-<footer>
-	<?php
-	include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
-	?>
-</footer>
-
-</html>
+<?php
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
+?>
