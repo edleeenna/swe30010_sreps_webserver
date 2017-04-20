fb7f23a9f6bb1a4fce518b39fe8de9ab2ac938d6
JohnVin <2050056@student.swin.edu.au>
2017-04-20 21:30:31 +1000

    Update manage.php
    Updated for new structure. John.

--- stock/manage.php
+++ stock/manage.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html>
-
-<header>
-	<title>Manage Stock - PHP - SRePS</title>
-	<nav class="nav-extended">
-		<?php
-		include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
-		include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
-		?>
-	</nav>
-</header>
-
-<main>
-	<body>
-		Manage Stock
-	</body>
-</main>
-
-<footer>
-	<?php
-	include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
-	?>
-</footer>
-
-</html>
+<?php
+  $pageTitle = "Manage Stock";
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
+?>
+    </nav>
+    <main>
+      Manage Stock
+    </main>
+<?php
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
+?>
