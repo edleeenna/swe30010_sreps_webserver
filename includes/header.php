84e5fe559fabd02dcb13ba64793761ea896020f6
JohnVin <2050056@student.swin.edu.au>
2017-04-20 20:31:18 +1000

    Update header.php
    Updated to be just the header. John.

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-	<div class="container">
-		<div class="nav-wrapper">
-			<a href="#!" class="brand-logo"><img src="/images/medicine-512x512.png" alt="" style="width:32px;height:32px;">PHP - SRePS</a>
-		</div>
-	</div>
-</body>
-
-</html>
+    <header>
+	    <div class="container">
+          <div class="nav-wrapper">
+            <a href="#!" class="brand-logo"><img src="/images/medicine-512x512.png" alt="" style="width:32px;height:32px;">PHP - SRePS</a>
+          </div>
+      </div>
+    </header>
