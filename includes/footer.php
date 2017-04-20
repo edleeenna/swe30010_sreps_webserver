30cd93aa0d4db663b1ce4e8a14367fdf37e2d1e8
JohnVin <2050056@student.swin.edu.au>
2017-04-20 20:39:12 +1000

    Update footer.php
    Updated Footer to just be a footer. John.

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,28 @@
-<!DOCTYPE html>
-<html>
-<footer class="page-footer">
-	<div class="container">
-		<div class="row">
-			<div class="col l6 s12">
-				<h5 class="white-text">PHP - SRePS</h5>
-				<p class="grey-text text-lighten-4">Pharmacy Sales Reporting and Prediction System.</p>
-			</div>
-			<div class="col l4 offset-l2 s12">
-				<h5 class="white-text">Links</h5>
-				<ul>
-					<li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a>
-					</li>
-					<li><a class="grey-text text-lighten-3" href="#!">Disclaimer</a>
-					</li>
-					<li><a class="grey-text text-lighten-3" href="/copyright.php">Copyright</a>
-					</li>
-					<li><a class="grey-text text-lighten-3" href="#!">Contact Us</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="footer-copyright">
-		<div class="container">
-			&copy 2017 Swinburne University of Technology
-		</div>
-	</div>
-</footer>
-
-</html>
+    <footer class="page-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">PHP - SRePS</h5>
+            <p class="grey-text text-lighten-4">Pharmacy Sales Reporting and Prediction System.</p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+              <li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a>
+              </li>
+              <li><a class="grey-text text-lighten-3" href="#!">Disclaimer</a>
+              </li>
+              <li><a class="grey-text text-lighten-3" href="/copyright.php">Copyright</a>
+              </li>
+              <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+          &copy; 2017 Swinburne University of Technology
+        </div>
+      </div>
+    </footer>
