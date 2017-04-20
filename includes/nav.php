756ba1fed57d8866a1f179c93681139dd1fa0b61
JohnVin <2050056@student.swin.edu.au>
2017-04-20 20:36:22 +1000

    Update nav.php
    Updated to be just the Nav section. John.

--- includes/nav.php
+++ includes/nav.php
@@ -1,28 +1,23 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-	<div class="container">
-		<div class="nav-wrapper">
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="/index.php">Home</a>
-				</li>
-				<li><a href="#!">Quick Sale</a>
-				</li>
-				<!--<li><a href="/sales/quick_sale.php">Quick Sale</a></li>-->
-				<li><a href="/search.php">Search</a>
-				</li>
-				<li><a href="/stock/manage.php">Manage Stock</a>
-				</li>
-				<li><a href="#!">Predictions</a>
-				</li>
-				<!--<li><a href="/stock/predictions.php">Predictions</a></li>-->
-				<li><a href="#!">Support</a>
-				</li>
-				<!--<li><a href="/support.php">Support</a></li>-->
-			</ul>
-		</div>
-	</div>
-</body>
-
-</html>
+    <nav>
+      <div class="container">
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="/index.php">Home</a>
+            </li>
+            <li><a href="#!">Quick Sale</a>
+            </li>
+            <!--<li><a href="/sales/quick_sale.php">Quick Sale</a></li>-->
+            <li><a href="/search.php">Search</a>
+            </li>
+            <li><a href="/stock/manage.php">Manage Stock</a>
+            </li>
+            <li><a href="#!">Predictions</a>
+            </li>
+            <!--<li><a href="/stock/predictions.php">Predictions</a></li>-->
+            <li><a href="#!">Support</a>
+            </li>
+            <!--<li><a href="/support.php">Support</a></li>-->
+          </ul>
+        </div>
+      </div>
+    </nav>
