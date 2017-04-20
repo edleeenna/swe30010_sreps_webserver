4bfaa2e0bc1b4ec1fa31cd1808ff5518bd5d6326
JohnVin <2050056@student.swin.edu.au>
2017-04-20 20:28:24 +1000

    Update head.php
    Updated to accomodate page changes but restore page independance. John.

--- includes/head.php
+++ includes/head.php
@@ -1,22 +1,44 @@
-<!DOCTYPE html>
+<?php
+  if (session_id()==''){
+    session_start();
+  }
+?>
+<!DOCTYPE HTML>
+<!--
+    * File Name    : <?php echo basename($_SERVER['PHP_SELF'])."\r\n";?>
+    * Author       : John V.
+    * Date Created : <?php echo date ("d F Y", filectime(basename($_SERVER['PHP_SELF'])))."\r\n";?>
+    * Version      : 1.0
+    * Last Modified: <?php echo date ("d F Y", filemtime(basename($_SERVER['PHP_SELF'])))."\r\n";?>
+    * Description  :
+-->
 <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="SIG Managment System">
+    <meta name="keywords" content="SIG Managment System">
+    <meta name="author" content="Adaptive Silicon">
 
-<head>
-	<link rel="icon" type="image/png" href="/images/medicine-32x32.png" sizes="32x32"/>
-	<link rel="icon" type="image/png" href="/images/medicine-16x16.png" sizes="16x16" />
 
-	<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link type="text/css" rel="stylesheet" href="/css/common.css"/>
-	<link type="text/css" rel="stylesheet" href="/css/materialize.min.css" media="screen,projection"/>
+    <link rel="icon" type="image/png" href="/images/medicine-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="/images/medicine-16x16.png" sizes="16x16" />
 
-	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="/js/materialize.min.js"></script>
-</head>
+    <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="/css/common.css"/>
+    <link type="text/css" rel="stylesheet" href="/css/materialize.min.css" media="screen,projection"/>
+<?php
+  // This line enables each page to add it's own extra CSS file, for page specific components.
+  if ($extra_css != "") echo '    <link type="text/css" rel="stylesheet" href="css/'.$extra_css.'"/>'.PHP_EOL;
+?>
 
-<body>
-	<?php
-	include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/header.php';
-	?>
-</body>
-
-</html>
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="/js/materialize.min.js"></script>
+<?php   
+  // This line enables each page to add it's own extra javascript file, for page specific components or functionality.
+  if ($extra_js != "") echo '    <script type="text/javascript" src="js/'.$extra_js.'"></script>'.PHP_EOL;
+?>
+  </head>
+  <body>
+<?php
+  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/header.php';
+?>
