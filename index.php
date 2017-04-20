61bd31d28579c687efeb469e71ee13bd65990d36
JohnVin <2050056@student.swin.edu.au>
2017-04-20 20:52:35 +1000

    Update index.php
    Added main tag back in. John.

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
-/*<main>
-
-  <body>*/
+/*
+  <body>
+*/
 ?>
-    <div style="text-align:center">
-      <p>
-        <a class="waves-effect waves-light btn-large disabled" href="sales/quick_sale.php">Quick Sale</a>
-        <a class="waves-effect waves-light btn-large" href="search.php">Search</a>
-        <a class="waves-effect waves-light btn-large" href="stock/manage.php">Manage Stock</a>
-      </p>
-      <p> <a class="waves-effect waves-light btn-large disabled" href="stock/predictions.php">Predictions</a> <a class="waves-effect waves-light btn-large disabled" href="support.php">Support</a> </p>
-    </div>
+    <main>
+      <div style="text-align:center">
+        <p>
+          <a class="waves-effect waves-light btn-large disabled" href="sales/quick_sale.php">Quick Sale</a>
+          <a class="waves-effect waves-light btn-large" href="search.php">Search</a>
+          <a class="waves-effect waves-light btn-large" href="stock/manage.php">Manage Stock</a>
+        </p>
+        <p> <a class="waves-effect waves-light btn-large disabled" href="stock/predictions.php">Predictions</a> <a class="waves-effect waves-light btn-large disabled" href="support.php">Support</a> </p>
+      </div>
+    </main>
 <?php
-/*  </body>
-</main>*/
+/*
+  </body>
+*/
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
 ?>
