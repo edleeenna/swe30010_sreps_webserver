a3cbcd898f1b77af5d69e6d99f7e49f0cb811a6a
a5c3nd3d <jrneeds@outlook.com>
2017-04-21 23:51:22 +1000

    Change to search page to align with the new page structure

--- search.php
+++ search.php
@@ -1,19 +1,25 @@
 <?php
+  // Variable to set the local (current) page title [NOT Site Title].
   $pageTitle = "Search";
 
   // Variable to assign extra css files into the head of the page. just the file name needs to go in the quotes.
   // All files should reside in the "css" folder. 
-  //$extra_css = "";
+  $extra_css = "";
   
   // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
   // All files should reside in the "js" folder.
-  //$extra_ja = "";
-  
-  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
-  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
-?>
-    </nav>
+  $extra_js = "";
 
+  include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
+/*
+  <body>
+*/
+?>
+<?php
+  include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/nav.php';
+?>
+    
+</nav>
     <main>
       Search
     </main>
