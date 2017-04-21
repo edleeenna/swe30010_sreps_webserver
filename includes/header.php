927601b15f4ee97117efc497eb727f1a6646b733
a5c3nd3d <jrneeds@outlook.com>
2017-04-21 23:40:06 +1000

    Change header to display individual page title

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
       <div class="container">
         <div class="nav-wrapper">
-          <a href="#!" class="brand-logo"><img src="/images/medicine-512x512.png" alt="" style="width:32px;height:32px;">PHP - SRePS</a>
+          <a href="#!" class="brand-logo"><img src="/images/medicine-512x512.png" alt="" style="width:32px;height:32px;"><?php if ($pageTitle <> "") echo $pageTitle; else echo $siteTitle;?></a>
         </div>
       </div>
