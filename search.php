5cd35ae892c42e7affd9ec06168ee3035e96ea04
JohnVin <2050056@student.swin.edu.au>
2017-04-21 13:58:45 +1000

    Update search.php
    Added variables for extra css and javascript files to the page. John.

--- search.php
+++ search.php
@@ -1,5 +1,14 @@
 <?php
   $pageTitle = "Search";
+
+  // Variable to assign extra css files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "css" folder. 
+  //$extra_css = "";
+  
+  // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "js" folder.
+  //$extra_ja = "";
+  
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
 ?>
