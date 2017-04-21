f65f29926a9b1ef9293d4546b8e3e5e4d3fe3aa5
a5c3nd3d <jrneeds@outlook.com>
2017-04-22 00:33:26 +1000

    Fix manage page to align with the set page structure. Add button links to add_stock, view_stock and edit_stock

--- stock/manage.php
+++ stock/manage.php
@@ -1,12 +1,34 @@
 <?php
+  // Variable to set the local (current) page title [NOT Site Title].
   $pageTitle = "Manage Stock";
-  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
-  include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
+
+  // Variable to assign extra css files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "css" folder. 
+  $extra_css = "";
+  
+  // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "js" folder.
+  $extra_js = "";
+
+  include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
+/*
+  <body>
+*/
 ?>
+<?php
+  // Nav would go here .. not on Index Page.
+  include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/nav.php';
+?>
+    
     </nav>
     <main>
-      Manage Stock
+ 		<a class="waves-effect waves-light btn-large disabled" href="/stock/view_stock.php">View Stock Item</a>
+        <a class="waves-effect waves-light btn-large" href="/stock/edit_stock.php">Edit Stock Item</a>
+        <a class="waves-effect waves-light btn-large" href="/stock/add_stock.php">Add Stock Item</a>
     </main>
 <?php
+/*
+  </body>
+*/
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
 ?>
