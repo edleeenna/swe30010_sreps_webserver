bb4ec018739eb91663f8093bd5befb9c2bd99eb7
jazonzos <1778064@student.swin.edu.au>
2017-04-21 14:52:21 +1000

    Update manage.php

--- stock/manage.php
+++ stock/manage.php
@@ -6,18 +6,6 @@
     <main>
       Manage Stock
     </main>
-<nav>
-  <div class="nav-wrapper">
-    <a href="#" class="brand-logo">PHP SRePS</a>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="./index.php">Home</a></li>
-      <li><a href="./addstock.php">Add Stock</a></li>
-      <li><a href="./edit_stock.php">Edit Stock</a></li>
-      <li><a href="./edit_stock_levels.php">Edit Stock Levels</a></li>
-      <li><a href="./displaystock.php">Display Stock</a></li>
-    </ul>
-  </div>
-</nav>
 <?php
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
 ?>
