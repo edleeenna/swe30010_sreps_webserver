e1538de702669b14726bfdcce31cc3ce125cd7cb
jazonzos <1778064@student.swin.edu.au>
2017-04-21 14:53:02 +1000

    Update manage.php

--- stock/manage.php
+++ stock/manage.php
@@ -3,6 +3,7 @@
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/head.php';
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/nav.php';
 ?>
+    </nav>
     <main>
       Manage Stock
     </main>
