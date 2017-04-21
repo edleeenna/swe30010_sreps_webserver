42c5759b5997f21d04a5671c2e76d42f54071488
JohnVin <2050056@student.swin.edu.au>
2017-04-21 14:05:38 +1000

    Update index.php
    Added Variables for pageTitle, and extra css and javascript files. John.

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+  // Variable to set the local (current) page title [NOT Site Title].
+  //$pageTitle = "";
+
+  // Variable to assign extra css files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "css" folder. 
+  //$extra_css = "";
+  
+  // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
+  // All files should reside in the "js" folder.
+  //$extra_ja = "";
+
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
 /*
   <body>
