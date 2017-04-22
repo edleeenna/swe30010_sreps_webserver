b3cad20182a6c796dea609d8d571334a15e22c32
JohnVin <2050056@student.swin.edu.au>
2017-04-22 11:36:10 +1000

    Update edit_stock.php
    Added some files for css and js for testing. John.

--- stock/edit_stock.php
+++ stock/edit_stock.php
@@ -4,11 +4,11 @@
 
   // Variable to assign extra css files into the head of the page. just the file name needs to go in the quotes.
   // All files should reside in the "css" folder. 
-  $extra_css = "";
+  $extra_css = "test.css";
   
   // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
   // All files should reside in the "js" folder.
-  $extra_js = "";
+  $extra_js = "test.js";
 
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
 /*
