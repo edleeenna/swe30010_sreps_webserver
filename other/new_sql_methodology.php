47e42a801be870b65149af238a3a56e98a0eef51
JohnVin <2050056@student.swin.edu.au>
2017-05-01 22:29:23 +1000

    Update new_sql_methodology.php

--- other/new_sql_methodology.php
+++ other/new_sql_methodology.php
@@ -21,20 +21,20 @@
   if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $conn->connect_error . PHP_EOL;
   }
-  // $sql = "SELECT * FROM stock";
-  // $result = $conn->query($sql);
-  // // Point to start of result set again.
-  // // $result->data_seek(0);
-  // while ($row = $result->fetch_assoc()) {
-  //   echo " Stock Id = " . $row['stock_id'] . "<br>".PHP_EOL;
-  // }
-  // //echo $row['stock_id'];
-
+  $sql = "SELECT * FROM stock";
+  $result = $conn->query($sql);
+  // Point to start of result set again.
+  // $result->data_seek(0);
+  while ($row = $result->fetch_assoc()) {
+    echo " Stock Id = " . $row['stock_supplier'] . "<br>".PHP_EOL;
+  } /* */
+  //echo $row['stock_id'];
+  /*
   $sql = "UPDATE stock SET stock_qty = 26 WHERE stock_id = 000001";
   if ($conn->query($sql) === true) {
     echo "Record updated successfully";
   }
   else {
     echo "Error updating record: (" . $mysqli->connect_errno . ") " . $conn->connect_error . PHP_EOL;
-  }
+  } /* */
 ?>
