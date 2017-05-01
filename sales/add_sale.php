354555c4ee29ddc53ef146d65d44778715b5c832
Elena D <edleeenna@hotmail.com>
2017-05-01 19:15:25 +1000

    Update add_sale.php

--- sales/add_sale.php
+++ sales/add_sale.php
@@ -8,7 +8,7 @@
   
   // Variable to assign extra javascript files into the head of the page. just the file name needs to go in the quotes.
   // All files should reside in the "js" folder.
-  //$extra_js = "";
+  $extra_js = "js/add_sales.js";
 
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
 /*
