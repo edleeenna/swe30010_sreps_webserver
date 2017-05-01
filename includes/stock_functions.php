2405234f87087fa6c004e3f3ae947ff871020989
JohnVin <2050056@student.swin.edu.au>
2017-05-01 22:13:12 +1000

    Update stock_functions.php

--- includes/stock_functions.php
+++ includes/stock_functions.php
@@ -232,23 +232,36 @@ SQL; */
     $php_stock_category_id    = cleanInput($_POST['html_stock_category_id']);
     $php_stock_barcode        = cleanInput($_POST['html_stock_barcode']);
     $php_stock_id             = cleanInput($_POST['html_stock_id']);
-    
+    /*
     $sql = <<<SQL
       UPDATE $sqltable 
       SET    stock_name='$php_stock_name',
              stock_description='$php_stock_description',
              stock_directions='$php_stock_directions',
              stock_ingredients='$php_stock_ingredients',
-             stock_price=$php_stock_price,
-             stock_cost_price=$php_stock_cost_price,
-             stock_qty=$php_stock_qty,
-             stock_target_min_qty=$php_stock_target_min_qty,
+             stock_price='$php_stock_price',
+             stock_cost_price='$php_stock_cost_price',
+             stock_qty='$php_stock_qty',
+             stock_target_min_qty='$php_stock_target_min_qty',
              stock_supplier='$php_stock_supplier',
              stock_supplier_order_code='$php_stock_supplier_code',
-             stock_category_id=$php_stock_category_id,
+             stock_category_id='$php_stock_category_id',
              stock_barcode='$php_stock_barcode'
       WHERE  stock_id=$php_stock_id;
-SQL;
+SQL; /* */
+    $sql = "UPDATE $sqltable SET stock_name='$php_stock_name',";
+    $sql .= " stock_description='$php_stock_description',";
+    $sql .= " stock_directions='$php_stock_directions',";
+    $sql .= " stock_ingredients='$php_stock_ingredients',";
+    $sql .= " stock_price='$php_stock_price',";
+    $sql .= " stock_cost_price='$php_stock_cost_price',";
+    $sql .= " stock_qty='$php_stock_qty',";
+    $sql .= " stock_target_min_qty='$php_stock_target_min_qty',";
+    $sql .= " stock_supplier='$php_stock_supplier',";
+    $sql .= " stock_supplier_order_code='$php_stock_supplier_code',";
+    $sql .= " stock_category_id='$php_stock_category_id',";
+    $sql .= " stock_barcode='$php_stock_barcode'";
+    $sql .= " WHERE stock_id=$php_stock_id;";
     //echo '<script type="text/javascript">alert("SQL Statment assembled.");</script>'.PHP_EOL;
     echo "Stock Id: ".$_POST['html_stock_id']."<br> SQL: ".$sql.'<br>'.PHP_EOL;
     
