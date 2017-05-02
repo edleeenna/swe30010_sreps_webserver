d65a3e465ac4044b26785662d8bc86b1b6b73808
JohnVin <2050056@student.swin.edu.au>
2017-05-02 19:32:39 +1000

    Update delete_stock.php
    Changed if test condition. John.

--- stock/delete_stock.php
+++ stock/delete_stock.php
@@ -21,11 +21,12 @@
   //echo "connect<br>".PHP_EOL;
   // Include functions for deleting stock. Could be made part of all functions for stock. eg: stock_func.php
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/stock_functions.php';
-  if (isset ($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST)) {
+  //if (isset ($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST)) {
+  if (!isset($_POST['html_stock_id']) && !isset($_GET['stock_id'])) {
     if ($debug) echo "1) Select item to delete.<br>".PHP_EOL;
 ?>
     <form id="stock_item" action="delete_stock.php" method="get">
-      <label>Select stock item to edit, by ID or Name.</label><br>
+      <label>Select stock item to delete, by ID.</label><br>
       <select class="browser-default" name="html_selected_id">
         <?php get_ID_list(); ?>
       </select> 
@@ -34,7 +35,7 @@
       <input type="reset" value="Reset">
     </form>
 <?php
-  } elseif (!(isset ($_GET) && $_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET))) {
+  } elseif (isset($_GET['stock_id'])) {
     if ($debug) echo "2) Display selected item prior to delete request.<br>".PHP_EOL;
       $php_stock = get_stock($_GET['stock_id']);
 ?>
@@ -48,43 +49,43 @@
           </div>
           <div class="input-field">
             <label for="html_stock_name">Item Name</label>
-            <input type="text" id="html_stock_name" name="html_stock_name" class="validate" value="<?php echo $php_stock['name'];?>">
+            <input readonly type="text" id="html_stock_name" name="html_stock_name" class="validate" value="<?php echo $php_stock['name'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_description">Item Description</label>
-            <textarea id="html_stock_description" name="html_stock_description" class="materialize-textarea"><?php echo $php_stock['description'];?></textarea>
+            <textarea readonly id="html_stock_description" name="html_stock_description" class="materialize-textarea"><?php echo $php_stock['description'];?></textarea>
           </div>
           <div class="input-field">
             <label for="html_stock_directions">Directions</label>
-            <textarea id="html_stock_directions" name="html_stock_directions" class="materialize-textarea"><?php echo $php_stock['directions'];?></textarea>
+            <textarea readonly id="html_stock_directions" name="html_stock_directions" class="materialize-textarea"><?php echo $php_stock['directions'];?></textarea>
           </div>
           <div class="input-field">
             <label for="html_stock_ingredients">Ingredients</label>
-            <textarea id="html_stock_ingredients" name="html_stock_ingredients" class="materialize-textarea"><?php echo $php_stock['ingredients'];?></textarea>
+            <textarea readonly id="html_stock_ingredients" name="html_stock_ingredients" class="materialize-textarea"><?php echo $php_stock['ingredients'];?></textarea>
           </div>
           <div class="input-field">
             <label for="html_stock_price">Item Price</label>
-            <input type="text" id="html_stock_price" name="html_stock_price" class="validate" value="<?php echo $php_stock['price'];?>">
+            <input readonly type="text" id="html_stock_price" name="html_stock_price" class="validate" value="<?php echo $php_stock['price'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_cost_price">Item Cost Price</label>
-            <input type="text" id="html_stock_cost_price" name="html_stock_cost_price" class="validate" value="<?php echo $php_stock['cost_price'];?>">
+            <input readonly type="text" id="html_stock_cost_price" name="html_stock_cost_price" class="validate" value="<?php echo $php_stock['cost_price'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_qty">Item Qty</label>
-            <input type="text" id="html_stock_qty" name="html_stock_qty" class="validate" value="<?php echo $php_stock['qty'];?>">
+            <input readonly type="text" id="html_stock_qty" name="html_stock_qty" class="validate" value="<?php echo $php_stock['qty'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_target_min_qty">Item Target</label>
-            <input type="text" id="html_stock_target_min_qty" name="html_stock_target_min_qty" class="validate" value="<?php echo $php_stock['target_min_qty'];?>">
+            <input readonly type="text" id="html_stock_target_min_qty" name="html_stock_target_min_qty" class="validate" value="<?php echo $php_stock['target_min_qty'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_supplier">Item Supplier</label>
-            <input type="text" id="html_stock_supplier" name="html_stock_supplier" class="validate" value="<?php echo $php_stock['supplier'];?>">
+            <input readonly type="text" id="html_stock_supplier" name="html_stock_supplier" class="validate" value="<?php echo $php_stock['supplier'];?>">
           </div>
           <div class="input-field">
             <label for="html_stock_supplier_code">Item Supplier Code</label>
-            <input type="text" id="html_stock_supplier_code" name="html_stock_supplier_code" class="validate" value="<?php echo $php_stock['supplier_order_code'];?>">
+            <input readonly type="text" id="html_stock_supplier_code" name="html_stock_supplier_code" class="validate" value="<?php echo $php_stock['supplier_order_code'];?>">
           </div>
           <div class="input-field">
             <label>Category Name</label>
@@ -95,7 +96,7 @@
           </div>
           <div class="input-field">
             <label for="html_stock_barcode">Item Barcode</label>
-            <input type="text" id="html_stock_barcode" name="html_stock_barcode" class="validate" value="<?php echo $php_stock['barcode'];?>">
+            <input readonly type="text" id="html_stock_barcode" name="html_stock_barcode" class="validate" value="<?php echo $php_stock['barcode'];?>">
           </div>
         </fieldset>
         <p>
@@ -106,7 +107,7 @@
     </div>
 <?php
   }
-  else{
+  elseif (isset($_POST['html_stock_id'])) {
     if ($debug) echo "3) Display item deleted confirmation message.<br>".PHP_EOL;
       //$succ = delete_stock($_POST);
   }
