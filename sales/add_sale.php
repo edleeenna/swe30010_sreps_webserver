d57fa22ba2d191212bfc16a79f15bfd09d3d2778
Elena D <edleeenna@hotmail.com>
2017-05-03 18:30:26 +1000

    Update add_sale.php

--- sales/add_sale.php
+++ sales/add_sale.php
@@ -38,16 +38,6 @@
         <?php get_ID_list(); ?>
       </select>
 	</div>
-		<?php
-		$q = intval($_GET['q']);
-		include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/db_connect.php';
-		$sql="SELECT * FROM sales WHERE id = '".$q."'";
-		$result = mysqli_query($conn,$sql);
-		while ($row = mysqli_fetch_assoc($result)) {
-			$row['stock_name'] = $stockName;
-			$row['stock_price'] = $stockPrice;
-		}
-		?>
 	<div class="input-field">
             <input readonly type="text" id="html_stock_orderlines_name" name="html_orderlines_name_datetime" value="<?php $stockName ?>" class="validate">
             <label for="html_stock_orderlines_name">Stock Name: </label>
