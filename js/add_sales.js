eaf8e47597c799f66ca267731f7f1e49feb91184
Elena D <edleeenna@hotmail.com>
2017-05-03 17:40:19 +1000

    Update add_sales.js

--- js/add_sales.js
+++ js/add_sales.js
@@ -26,6 +26,28 @@ function getDateTime() {
 }
 
 
+function showStock(id) {
+    if (id == id) {
+        document.getElementById("error_message").innerHTML = "";
+        return;
+    } else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("error_message").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","add_sale.php?q="+id,true);
+        xmlhttp.send();
+    }
+}
+
 function init(){
   'use strict';
 
