988cd4e46307ee5dd7380f4426a4b1d67d371483
JohnVin <2050056@student.swin.edu.au>
2017-05-04 14:42:02 +1000

    Update view_all_stock.php
    Adjusted for making more readable HTML output. John.

--- stock/view_all_stock.php
+++ stock/view_all_stock.php
@@ -11,9 +11,6 @@
   //$extra_js = "";
 
   include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/head.php';
-/*
-  <body>
-*/
 ?>
 <?php
 	include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/nav.php';
@@ -25,11 +22,10 @@
 include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/db_connect.php';
 $listAllQuery = "select * FROM stock";
       $listResult = mysqli_query($conn, $listAllQuery);
-echo "<form method='post' action='stockexportprocess.php'>";
-echo " <p><input type='submit' id='submit' value='Export to CSV'></p>";
-echo "</form>";
-echo "<br>";
-echo "<table  class=\"striped\" border=\"1\">";
+echo "<form method='post' action='stockexportprocess.php'>".PHP_EOL;
+echo " <p><input type='submit' id='submit' value='Export to CSV'></p>".PHP_EOL;
+echo "</form><br>".PHP_EOL;
+echo "<table  class=\"striped\" border=\"1\">".PHP_EOL;
      echo "<tr>"
         ."<th scope=\"col\">Stock Id</th>"
         ."<th scope=\"col\">Stock Name</th>"
@@ -46,7 +42,7 @@ echo "<table  class=\"striped\" border=\"1\">";
         ."<th scope=\"col\">Barcode</th>"
         ."<th scope=\"col\"></th>"
         ."<th scope=\"col\"></th>"
-        ."</tr>";
+        ."</tr>".PHP_EOL;
 
         while ($row = mysqli_fetch_assoc($listResult)) {
           echo "<tr>";
@@ -65,19 +61,16 @@ echo "<table  class=\"striped\" border=\"1\">";
           echo "<td>", $row["stock_bar_code"], "</td>";
           echo '<td><button onclick="window.location.href=\'/stock/edit_stock.php?stock_id='.$row["stock_id"].'\'" title="Edit item '.$row["stock_id"].'">Edit&hellip;</button></td>';
           echo '<td><button onclick="window.location.href=\'/stock/view_stock.php?stock_id='.$row["stock_id"].'\'" title="View item '.$row["stock_id"].'">View&hellip;</button></td>';
-          echo "</tr>";
+          echo "</tr>".PHP_EOL;
 
         }
-        echo "</table>";
+        echo "</table>".PHP_EOL;
 
         mysqli_free_result($listResult);
 
         mysqli_close($conn);
 ?>
-</main>
+    </main>
 <?php
-/*
-  </body>
-*/
   include $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/tail.php';
 ?>
