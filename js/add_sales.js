95162587214677c7ebb2153d215495d732c552ea
Elena D <edleeenna@hotmail.com>
2017-05-06 01:43:09 +1000

    Update add_sales.js

--- js/add_sales.js
+++ js/add_sales.js
@@ -32,27 +32,9 @@ function getPrice(qty, price){
     
 }
 
-function showStock(id) {
-    if (id == 0) {
-        document.getElementById("error_message").innerHTML = "Something went wrong :(";
-        return;
-    } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("error_message").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","add_sale.php?q="+id,true);
-        xmlhttp.send();
-    }
-} 
+$(document).ready(function() {
+    Materialize.updateTextFields();
+  });
 
 function init(){
   'use strict';
