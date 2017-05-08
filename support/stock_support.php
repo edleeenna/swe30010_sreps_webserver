9402eaf63823847130983a3ef9b511e9617619f2
Elena D <edleeenna@hotmail.com>
2017-05-08 19:21:50 +1000

    Update stock_support.php

--- support/stock_support.php
+++ support/stock_support.php
@@ -122,10 +122,35 @@
           </div>
                      
         </div>
-      <h3>Delete Stock Item</h3>
-      Change this text later. Screen shot, with description of what the parts are and for. Description of how you would use them.
-      Perhaps take them through from start to finish, how to do a complete run.
-      </div>
+       <div class="row">
+          <div class="col s12">
+             <h3>Delete Stock Item</h3>
+            <p>This will take you through how to delete a stock item.</p>
+            </div>
+          
+          <div class="row">
+          <div class="col s2.5">
+            <img class="responsive-img materialboxed" src="/images/navtomanagestock.png" alt="how to nav to managestock" />
+            <p>From the home page, click on "Manage Stock" as shown in the image above. This will take you to the "Manage Stock" page and will present you with a few options to choose from</p>
+          </div>
+             <div class="col s2.5">
+            <img class="responsive-img materialboxed" src="/images/navtodeletestock.png" alt="how to nav to view all stock" />
+               <p>You should now be presented with options related to managing stock. From here you want to click on "Delete Stock".</p>
+          </div>
+             <div class="col s2.5">
+            <img class="responsive-img materialboxed" src="/images/selectitemtodelete.png" alt="how to view all stock" />
+               <p>On this page you will be able to select the item ID of the stock you want to Delete. Choose and item and click "submit". If you make a mistake, you can click on "reset" and start again</p>
+          </div>
+              <div class="col s2.5">
+            <img class="responsive-img materialboxed" src="/images/ConfirmDelete.png" alt="how to view all stock" />
+               <p>You should now be able to view the details of the selected stock item</p>
+          </div>
+             <div class="col s2.5">
+            <img class="responsive-img materialboxed" src="/images/ConfirmDelete.png" alt="how to view all stock" />
+               <p>You should now be able to view the details of the selected stock item</p>
+          </div>
+                     
+        </div>
         </main>
 <?php
   //Ending of body tag is in tail.php
