dd606964f1bb825bb52e71e66e8b6fb7d974ef9d
Elena D <edleeenna@hotmail.com>
2017-05-10 18:04:59 +1000

    Update sales_support.php

--- support/sales_support.php
+++ support/sales_support.php
@@ -17,6 +17,7 @@
     </nav>
 
     <main>
+	     <div class="container">
       Work in progress.
       <h2>Sales Support</h2>
       Support for how to use the sections of the sales system.
@@ -29,9 +30,32 @@
 	    <h3>View Sale Item</h3>
       Change this text later. Screen shot, with description of what the parts are and for. Description of how you would use them.
       Perhaps take them through from start to finish, how to do a complete run.
-	    <h3>View All Sale Items</h3>
-      Change this text later. Screen shot, with description of what the parts are and for. Description of how you would use them.
-      Perhaps take them through from start to finish, how to do a complete run.
+	 
+               <div class="row">
+          <div class="col s12">
+             <h3>View All Sales</h3>
+            <p>This will take you through how to navigate to and how to view all stock from the database.</p>
+            </div>
+          
+          <div class="row">
+          <div class="col s3">
+            <img class="responsive-img materialboxed" src="/images/navtomanagesales.png" alt="how to nav to managesales" />
+            <p></p>
+          </div>
+             <div class="col s3">
+            <img class="responsive-img materialboxed" src="/images/navtoviewallsales.png" alt="how to nav to view all stock" />
+               <p></p>
+          </div>
+             <div class="col s3">
+            <img class="responsive-img materialboxed" src="/images/viewallsales.png" alt="how to view all stock" />
+               <p></p>
+          </div>
+		    <div class="col s3">
+            <img class="responsive-img materialboxed" src="/images/viewsalefromallsales.png" alt="how to view all stock" />
+               <p></p>
+          </div>
+                     
+        </div>
     </main>
 <?php
   //Ending of body tag is in tail.php
