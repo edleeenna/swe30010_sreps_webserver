95da65483c68e7a77009d3a1fb8a4262f83a5fa0
JohnVin <2050056@student.swin.edu.au>
2017-05-12 09:22:11 +1000

    Update utilities.php
    Added some fields names to the list. John.

--- includes/utilities.php
+++ includes/utilities.php
@@ -3,23 +3,26 @@
 function get_column_name($val) {
 	switch ($val) {
 		case "stock_id":
-			return "ID";
-		case "stock_name":			      return "Name";
-		case "stock_description":	      return "Description";
-		case "stock_directions":	      return "Directions";
-		case "stock_ingredients":  	      return "Ingredients";
-		case "stock_price":			      return "Price ($)";
-		case "stock_cost_price":	      return "Cost Price ($)";
-		case "stock_qty":			      return "Quantity";
-		case "stock_target_min_qty":      return "Minimum Quantity";
-		case "stock_supplier":            return "Supplier";
-		case "stock_supplier_order_code": return "Order Code";
-		case "stock_category_id":         return "Category ID";
-		case "stock_barcode":             return "Barcode";
-		case "category_name":             return "Category";
-		case "sale_id":                   return "Sale ID";
-		case "sale_datetime":                   return "Sale DateTime";
-		case "sale_total":                return "Total";
+				return "ID";
+		case "stock_name":			return "Name";
+		case "stock_description":		return "Description";
+		case "stock_directions":		return "Directions";
+		case "stock_ingredients":		return "Ingredients";
+		case "stock_price":			return "Price ($)";
+		case "stock_cost_price":		return "Cost Price ($)";
+		case "stock_qty":			return "Quantity";
+		case "stock_target_min_qty":		return "Minimum Quantity";
+		case "stock_supplier":			return "Supplier";
+		case "stock_supplier_order_code":	return "Order Code";
+		case "stock_category_id":		return "Category ID";
+		case "stock_barcode":			return "Barcode";
+		case "category_name":			return "Category";
+		case "sale_id":				return "Sale ID";
+		case "sale_datetime":			return "Sale DateTime";
+		case "sale_total":			return "Total";
+		case "subtotal":			return "Sub-Total";
+		case "orderline_stock_id":		return "Stock Id";
+		case "orderline_qty":			return "Quantity";
 		default:
 			return "UNDEFINED";
 	}
