ae669b52c1f9a786e38c87ac56c2feda109287a0
JohnVin <2050056@student.swin.edu.au>
2017-05-12 10:07:32 +1000

    Update reports.php

--- reports.php
+++ reports.php
@@ -38,50 +38,49 @@
 		
 		switch ($_GET['type']){
 			case "sales_by_week":
-			$pageTitle = "Sales by week";
+			  $pageTitle = "Sales by week";
 				echo "<p>Sales by week report</p>".PHP_EOL;
 				echo "<p>UNIMPLEMENTED</p>".PHP_EOL;
 				
 				$allSaleQuery = ""; /* */
-          include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/db_connect.php';
-          include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/stock_functions.php';
-          include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/Paginator.class.php';
-          include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/utilities.php';
-          $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
-          $page       = ( isset( $_GET['page'] ) )  ? $_GET['page']  : 1;
-          $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 3;
-          $query=<<<SQL
-          SELECT sale_datetime, orderline_stock_id, stock_name, orderline_qty, (orderline_qty * orderline_price) as subtotal FROM orderlines
-          INNER JOIN sales ON orderline_sale_id = sale_id
-          WHERE sale_datetime >= '2017/03/07' AND sale_datetime <= DATE_ADD('2017/03/08', INTERVAL 1 DAY)
-          ORDER BY sale_datetime, orderline_stock_id
+        include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/db_connect.php';
+        include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/stock_functions.php';
+        include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/Paginator.class.php';
+        include $_SERVER[ 'DOCUMENT_ROOT' ].'/includes/utilities.php';
+        $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
+        $page       = ( isset( $_GET['page'] ) )  ? $_GET['page']  : 1;
+        $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 3;
+        $query=<<<SQL
+        SELECT sale_datetime, orderline_stock_id, stock_name, orderline_qty, (orderline_qty * orderline_price) as subtotal FROM orderlines
+        INNER JOIN sales ON orderline_sale_id = sale_id
+        WHERE sale_datetime >= '2017/03/07' AND sale_datetime <= DATE_ADD('2017/03/08', INTERVAL 1 DAY)
+        ORDER BY sale_datetime, orderline_stock_id
 SQL;
-                
-          $Paginator  = new Paginator( $conn, $query );
-          $results    = $Paginator->getData( $limit, $page );
-      		$lastdate   = "";
+        $Paginator  = new Paginator( $conn, $query );
+        $results    = $Paginator->getData( $limit, $page );
+        $lastdate   = "";
 ?>
     <div class="container">
 		  <table class="striped" border="1">
 			  <tr>
 <?php	
-			foreach($results->fields as $field)    {
+			  foreach($results->fields as $field)    {
 ?>
 				  <th><?php echo get_column_name($field); ?></th>
 <?php
-			}
+			  }
 				//<th></th><th></th></tr>
 ?>
 <?php		
-      for( $i = 0; $i < count( $results->data ); $i++ ) {
+        for( $i = 0; $i < count( $results->data ); $i++ ) {
 ?>
 			  <tr>
 <?php
-			  foreach($results->data[$i] as $cell) {
+			    foreach($results->data[$i] as $cell) {
 ?>
-				  <td><?php echo $cell; ?></td>
+          <td><?php echo $cell; ?></td>
 <?php
-			  }
+			    }
 				//<td><a class="waves-effect waves-light btn" href="/sales/edit_sale.php?sale_id=<? php echo $results->data[$i]["sale_id"]? >" title="Edit item <? php echo $results->data[$i]["sale_id"]? >">Edit</a></td>
 				//<td><a class="waves-effect waves-light btn" href="/sales/view_sale.php?sale_id=<? php echo $results->data[$i]["sale_id"]? >" title="View item <? php echo $results->data[$i]["sale_id"]? >">View</a></td>
 ?>
@@ -94,10 +93,10 @@ SQL;
 			  /* <tr>
 				  <td></td>
 			  </tr> */
-      }  
+        }  
 ?>
 <?php
-    }
+    //}
 ?>
 		</table>
 </div>
@@ -200,7 +199,7 @@ SQL;
 			default:
 				header("Location:/reports.php");
 				break;
-		}
+		} //End Case
 
 		
 		// Floating menu button
