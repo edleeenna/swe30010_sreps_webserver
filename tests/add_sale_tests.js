4997934f96c5a2eaaed09e81c4b875f078799b9d
Elena D <edleeenna@hotmail.com>
2017-05-14 11:55:27 +1000

    Testing for Distinction Tasks

---
+++ tests/add_sale_tests.js
@@ -0,0 +1,12 @@
+/*
+* Author: Elena Deen, 7353065
+* Target: run_add_sale_tests.html
+* Purpose: To test functional completeness for Distinction Task 10.7 - Quality Review
+* Created: 14/05/2017
+* Last updated: 14/05/2017
+* Credits: QUnit
+*/
+
+QUnit.test("Hello Test", function(assert){
+  assert.ok(1 == "1", "Passed!");
+})
