0b02f9fccc08cab020b974a374b73726c9332460
Elena D <edleeenna@hotmail.com>
2017-05-14 12:11:18 +1000

    Update add_sale_tests.js

--- tests/add_sale_tests.js
+++ tests/add_sale_tests.js
@@ -7,6 +7,13 @@
 * Credits: QUnit
 */
 
+var addSale = require('./js/add_sale.js');
+
 QUnit.test("Hello Test", function(assert){
   assert.ok(1 == "1", "Passed!");
-})
+});
+
+QUnit.test("Add Price Test", function(assert){
+  var price = addSale.getPrice(1, 3);
+  assert.true(price, 3);
+});
